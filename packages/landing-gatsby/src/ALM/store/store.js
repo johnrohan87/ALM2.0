012fb46ff4382ea49e8ff5d602ab2f3e14eeb230
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-04-23 22:37:55 -0400

    testing redux build err

--- packages/landing-gatsby/src/ALM/store/store.js
+++ packages/landing-gatsby/src/ALM/store/store.js
@@ -1,12 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit"
-import apiSlice from "./apiSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import api from "./apiSlice";
 
 const store = configureStore({
   reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
+    [api.reducerPath]: api.reducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
-})
+    getDefaultMiddleware().concat(api.middleware),
+});
 
-export default store
+export default store;
