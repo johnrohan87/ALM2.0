1ec20b16d78b86d11579bb6070d14e84d4c36e00
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-04-24 17:01:22 -0400

    testing rtk query seems to be working

--- packages/landing-gatsby/src/ALM/store/api.js
+++ packages/landing-gatsby/src/ALM/store/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@rtk-incubator/rtk-query';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://your-auth0-domain.com/api/v2/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.GATSBY_AUTH0_DOMAIN+'/api/v2/users' }),
   endpoints: (builder) => ({
     getRoles: builder.query({
       query: () => '/roles',
