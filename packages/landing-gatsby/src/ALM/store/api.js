b5e1195d8b2f993b85c1220619bb50e823f1ace5
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-04-24 21:22:04 -0400

    testing

--- packages/landing-gatsby/src/ALM/store/api.js
+++ packages/landing-gatsby/src/ALM/store/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.GATSBY_AUTH0_DOMAIN+'/api/v2/users' }),
+  baseQuery: fetchBaseQuery({ baseUrl: `https://${process.env.GATSBY_AUTH0_DOMAIN}/api/v2/users` }),
   endpoints: (builder) => ({
     getRoles: builder.query({
       query: () => '/roles',
