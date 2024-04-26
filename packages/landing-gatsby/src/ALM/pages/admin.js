5028a5d92f701e81811e872015cfcbd4d905bcd4
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-04-25 21:23:35 -0400

    testing

--- packages/landing-gatsby/src/ALM/pages/admin.js
+++ packages/landing-gatsby/src/ALM/pages/admin.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { login, logout, isAuthenticated } from "../utils/auth";
 import AdminPanel from '../components/AdminPanel';
 
 const Admin = () => {
-  
+  const isBrowser = typeof window !== "undefined";
+
+  useEffect(() => {
+    if (!isBrowser) {
+      return;
+    }
+    if (!isAuthenticated()) {
+      login();
+    }
+  }, [isBrowser]);
+
+  if (!isAuthenticated()) {
+    return null; 
+  }
 
   return (
     <div>
