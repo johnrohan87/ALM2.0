68e9323d5ebbb5142ef7b98c839c4b77f07b95e9
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-04-26 21:59:51 -0400

    testing

--- packages/landing-gatsby/src/ALM/components/AdminPanel.js
+++ packages/landing-gatsby/src/ALM/components/AdminPanel.js
@@ -1,35 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useGetRolesQuery } from '../store/api';  
+import { useGetRolesQuery } from '../store/api';
 
 const AdminPanel = () => {
   const { getAccessTokenSilently, isAuthenticated } = useAuth0();
   const [token, setToken] = useState(null);
-  const { data: roles, error, isLoading, refetch } = useGetRolesQuery(undefined, {
+  const {
+    data: roles,
+    error,
+    isLoading,
+    refetch
+  } = useGetRolesQuery(undefined, {
     skip: !isAuthenticated || !token,
   });
 
   useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: 'https://YOUR_AUTH0_DOMAIN/api/v2/',
+          scope: 'read:roles'
+        });
+        setToken(accessToken);
+        console.log("Access Token:", accessToken);
+        refetch({
+          headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+          }
+        });
+      } catch (error) {
+        console.error('Error fetching access token:', error);
+        setToken(null);
+      }
+    };
+
     if (isAuthenticated) {
-      const fetchToken = async () => {
-        try {
-          const accessToken = await getAccessTokenSilently();
-          setToken(accessToken);
-          //console.log(accessToken)
-          refetch({ headers: { Authorization: `Bearer ${accessToken}` }, 
-          'Content-Type': 'application/json' });
-        } catch (error) {
-          console.error('Error fetching access token:', error);
-          setToken(null);
-        }
-      };
       fetchToken();
     }
   }, [getAccessTokenSilently, isAuthenticated, refetch]);
 
   if (isLoading) return <p>Loading roles...</p>;
   if (error) return <p>Error fetching roles: {error.message}</p>;
-  console.log(roles)
+
   const isAdmin = roles?.some(role => role.name === 'admin');
 
   return (
