511f6b8bc68301cff4b857f37c6b6d9a15bfff99
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-05-01 18:24:13 -0400

    testing revamp auth0 and rtk fetch

--- packages/landing-gatsby/src/ALM/utils/wrap-with-providers.js
+++ packages/landing-gatsby/src/ALM/utils/wrap-with-providers.js
@@ -7,17 +7,21 @@ import { store } from '../store/store';
 
 const onRedirectCallback = (appState) => navigate(appState?.returnTo || '/');
 
-export const wrapRootElement = ({ element }) => (
-  <Auth0Provider
-    domain={process.env.GATSBY_AUTH0_DOMAIN}
-    clientId={process.env.GATSBY_AUTH0_CLIENT_ID}
-    redirectUri={typeof window !== 'undefined' ? window.location.origin : ''}
-    onRedirectCallback={onRedirectCallback}
-    audience={process.env.GATSBY_AUTH0_AUDIENCE}
-    scope="openid profile email"
-  >
-    <ReduxProvider store={store}>
-      {element}
-    </ReduxProvider>
-  </Auth0Provider>
-);
+export const wrapRootElement = ({ element }) => {
+  console.assert(store != null, "Redux store is not initialized");
+
+  return (
+    <Auth0Provider
+      domain={process.env.GATSBY_AUTH0_DOMAIN}
+      clientId={process.env.GATSBY_AUTH0_CLIENT_ID}
+      redirectUri={typeof window !== 'undefined' ? window.location.origin : ''}
+      onRedirectCallback={onRedirectCallback}
+      audience={process.env.GATSBY_AUTH0_AUDIENCE}
+      scope="openid profile email"
+    >
+      <ReduxProvider store={store}>
+        {element}
+      </ReduxProvider>
+    </Auth0Provider>
+  );
+};
