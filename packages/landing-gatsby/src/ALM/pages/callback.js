3746a3a45d3746d0561d394a00b928304c79e183
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-05-02 23:31:26 -0400

    testing working local push

--- packages/landing-gatsby/src/ALM/pages/callback.js
+++ packages/landing-gatsby/src/ALM/pages/callback.js
@@ -1,10 +1,17 @@
-import React from "react"
-import { handleAuthentication } from "../utils/auth"
+import React, { useEffect } from "react";
+import { navigate } from "gatsby";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Callback = () => {
-  handleAuthentication()
+  const { isAuthenticated, isLoading } = useAuth0();
 
-  return <p>Loading...</p>
-}
+  useEffect(() => {
+    if (!isLoading && isAuthenticated) {
+      navigate("/account");
+    }
+  }, [isLoading, isAuthenticated]);
 
-export default Callback
+  return <p>Loading...</p>;
+};
+
+export default Callback;
