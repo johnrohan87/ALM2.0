c93a4b348c321858a08b1d3c4fc2c140b1582559
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-05-02 23:34:49 -0400

    testing working local push

--- packages/landing-gatsby/gatsby-browser.js
+++ packages/landing-gatsby/gatsby-browser.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { navigate } from "gatsby";
 import { Provider } from 'react-redux';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { configureStore } from '@reduxjs/toolkit';
@@ -12,13 +13,17 @@ export const store = configureStore({
     getDefaultMiddleware().concat(api.middleware),
 });
 
+const onRedirectCallback = (appState) => {
+  navigate(appState?.returnTo || "/account");
+};
+
 export const wrapRootElement = ({ element }) => {
   return (
     <Auth0Provider
       domain={process.env.GATSBY_AUTH0_DOMAIN}
       clientId={process.env.GATSBY_AUTH0_CLIENT_ID}
-      redirectUri={typeof window !== 'undefined' ? window.location.origin + "/callback" : ''}
-      onRedirectCallback={process.env.GATSBY_AUTH0_CALLBACK}
+      redirectUri={typeof window !== 'undefined' ? process.env.GATSBY_AUTH0_CALLBACK : ''}
+      onRedirectCallback={onRedirectCallback}
       audience={process.env.GATSBY_AUTH0_AUDIENCE}
       scope="openid profile email"
     >
