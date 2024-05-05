867223506e481682bce59ca63ee92ab92ead26db
johnrohan87 <62908377+johnrohan87@users.noreply.github.com>
2024-05-05 16:51:31 -0400

    testing wrong return to page bug

--- packages/landing-gatsby/src/pages/index.js
+++ packages/landing-gatsby/src/pages/index.js
@@ -3,7 +3,7 @@ import { Router } from '@reach/router';
 
 import Home from '../ALM/pages/hosting';
 //import Portfolio from '../ALM/pages/portfolio';
-//import Login from '../ALM/pages/login';
+import Login from '../ALM/pages/login';
 import Account from '../ALM/pages/account';
 import Admin from '../ALM/pages/admin';
 
@@ -11,8 +11,8 @@ const IndexRoutes = () => {
   return (
       <Router>
         <Home path="/" />
-        {/*<Portfolio path="/portfolio" />
-        <Login path="/login" />*/}
+        {/*<Portfolio path="/portfolio" />*/}
+        <Login path="/login" />
         <Admin path="/admin" />
         <Account path="/account"/>
       </Router>
